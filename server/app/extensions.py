82c59d3d4e4324d7e1096b0360b1c32c325df96a
Rohan Babbar <rohanbabbar2003@gmail.com>
2025-07-07 00:47:31 +0530

    Update extensions.py

--- server/app/extensions.py
+++ server/app/extensions.py
@@ -7,12 +7,9 @@ mongo = PyMongo()
 def init_mongo(app):
     load_dotenv()
     uri = os.getenv("MONGO_URI")
-    print("Loaded MONGO_URI:", uri)  # Debug: see if env is loaded
 
     if not uri:
         raise Exception("MONGO_URI not found in .env")
 
     app.config["MONGO_URI"] = uri
     mongo.init_app(app)
-
-    print("Mongo DB in init_mongo:", mongo.db)  # Debug again
